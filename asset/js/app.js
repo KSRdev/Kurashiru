0568666cb0053cf9f7494df6467fa3a670569e29
KSRdev <simoneradaelli@yahoo.com>
2022-03-25 16:49:48 +0000

    contact solved

--- asset/js/app.js
+++ asset/js/app.js
@@ -49,15 +49,20 @@ if (navClose) {
         navMenu.classList.remove('show-menu')
     })
 }
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
-/*==================== SCROLL SMOOTH ====================*/
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-var $root = $('html, body');
-
-$('a[href^="#"]').click(function() {
-    $root.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-
-    return false;
-});
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 400) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
