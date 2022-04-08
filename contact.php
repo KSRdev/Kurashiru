4d64b6c3f8fb540218feca05ee3980b92489d414
KSRdev <simoneradaelli@yahoo.com>
2022-04-08 11:00:03 +0100

    contact

--- contact.php
+++ contact.php
@@ -1,10 +1,23 @@
-<?php $email = $_POST['email'];
-$subjects = $_POST['subject'];
-$text = $_POST['text'];
-$formcontent="From: $subject \n Message: $text";
-$recipient = "simoneradaelli@yahoo.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+
+   
+<?php
+    //Component input user
+    $visitor_email = $_POST['email'];
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+
+    //Nama Email Website (Dari)
+    $email_form = 'simoneradaelli@yahoo.com';
+    $email_subject = "New Form Submission"; //Subject email yang dikirim
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+    
+    //Email Penerima (Ke)
+    //Masuknya ke akun gmail Cindy
+    $to = "simoneradaelli@yahoo.com";
+    $headers = "From: $email_form \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
 ?>
